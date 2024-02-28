a7c0a8b6318ba46b0b92ecbc863a3e9585a78989
agraham02 <acgraham02@gmail.com>
2024-02-27 20:11:42 -0500

    Update Nav design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import "../styles/Nav.css";
 
 export default function Nav() {
     return (
-        <nav className="flex justify-between px-3 py-2">
+        <nav className="flex justify-between px-3 py-2 fixed top-0 z-50 border w-full">
             <div className="border-2 border-black rounded-md p-2 px-6 cursor-pointer">
                 <div>AG</div>
             </div>
@@ -18,7 +18,7 @@ export default function Nav() {
 
 function NavLink({ text }) {
     return (
-        <div className="px-4 h-4/5 items-center flex cursor-pointer">
+        <div className="px-4 h-4/5 items-center flex cursor-pointer hover:text-sky-700">
             {text}
         </div>
     );
