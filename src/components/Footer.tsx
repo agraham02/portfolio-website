2d0fd202b5f03a6a37f637cb240a00e4ab5f69e5
agraham02 <acgraham02@gmail.com>
2024-03-10 03:03:21 -0400

    Update Header and Footer css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 p-4">
-            <div className="w-full mx-auto max-w-screen-xl  md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 p-3">
+            <div className="w-full mx-auto max-w-screen-xl whitespace-nowrap flex flex-wrap justify-between items-center">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mx-2">
                     © 2024{" "}
                     <a
                         href="https://ahmadgraham.me/"
@@ -15,7 +14,7 @@ export default function Footer() {
                     </a>
                     . All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <ul className="flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 mx-2">
                     <li>
                         <a href="#" className="hover:underline">
                             Back to top
@@ -23,12 +22,12 @@ export default function Footer() {
                     </li>
                 </ul>
             </div>
-            <hr className="my-3 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                Created by{" "}
+            <hr className="my-3 border-gray-200 sm:mx-auto dark:border-gray-700" />
+            <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400 flex justify-center">
+                Created by {" "} 
                 <a
                     href="https://ahmadgraham.me/"
-                    className="hover:underline"
+                    className="hover:underline mx-1"
                     target="_blank"
                 >
                     Ahmad Graham
