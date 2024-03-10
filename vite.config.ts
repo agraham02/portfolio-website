54846a590ba6d667e09c1bdff27806cc64501298
agraham02 <acgraham02@gmail.com>
2024-03-10 03:15:08 -0400

    Update config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://github.com/agraham02/portfolio-website",
+    base: "/portfolio-website",
     plugins: [react()],
 });
