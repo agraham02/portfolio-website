8caee52fff23ce86f1370056f240b54ce19e6fd0
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-07-01 13:22:32 -0400

    feat: remove introductory section from timeline component

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -37,16 +37,6 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
             ref={containerRef}
         >
-            <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-                <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-                    My Journey
-                </h2>
-                <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-                    A timeline of how I went from discovering code in high
-                    school to building full-stack apps and solving real-world
-                    problems.
-                </p>
-            </div>
 
             <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
                 {data.map((item, index) => (
