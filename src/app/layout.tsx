290e762df08428b9d70f9e86ecdbbbfe7be4e377
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-08-22 11:03:39 -0400

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConditionalLayout from "@/components/ConditionalLayout";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
             >
                 <ConditionalLayout>
                     {children}
+                    <Analytics />
                 </ConditionalLayout>
             </body>
         </html>
