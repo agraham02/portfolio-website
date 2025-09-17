fa7f1706d91814761cf6cb649007a4906b622ff3
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-09-17 16:56:49 -0400

    chore: update tailwindcss and postcss dependencies; refactor postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.mjs
+export default {
+    plugins: {
+        "@tailwindcss/postcss": {},
+    },
 };
-
-export default config;
